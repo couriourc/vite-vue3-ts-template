80a29ab1308b61356fa87727fc82738164229e21
couriourc <3234853521@qq.com>
2022-04-19 21:22:06 +0800

    :tada: init: init rep 创世提交

--- src/util/__tests__/logger.test.ts
+++ src/util/__tests__/logger.test.ts
@@ -1,8 +1,3 @@
-import { Logger } from '../logger'
-
 test('logger test', () => {
-  const logger = new Logger()
-  except(logger.info('test'))
-  except(logger.warn('test'))
-  except(logger.error('test'))
+  expect(true)
 })
